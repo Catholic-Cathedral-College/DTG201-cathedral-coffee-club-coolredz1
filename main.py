1cd9eda706021757e6d14500852138236e348c5a
LawrenceV1 <110138752+coolredz1@users.noreply.github.com>
2022-09-13 22:10:02 +0000

    I added the menu and the name of my caffe

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 
+from time import sleep
+
+#deffine the menu so I can print the menu with ease and not have to write the whole thing again and again. 
+
+def menu (): 
+  print("============================")
+  print("|Flat White           $3.00|")
+  print("|Cappuccino           $3.00|")
+  print("|Latte                $3.50|")
+  print("|Decaf                $3.00|")  
+  print("|Hot Chocolatte       $4.00|")
+  print("============================")
+  
+#print the title here, then print the menu after it. 
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("             BUY MY BROWN LIQUED                ")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+menu ()
+
+#make an input about what type of coffee they would like 
+input(print("What type of drink would you like to buy"))
+
