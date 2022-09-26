a844467d6a835b0d2ec5a939f5a300617529c6b4
LawrenceV1 <110138752+coolredz1@users.noreply.github.com>
2022-09-26 22:31:41 +0000

    something I did wrong with the name.

--- main.py
+++ main.py
@@ -29,39 +29,40 @@ print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 menu ()
 
 #make an input about what type of coffee they would like 
+order = input("What type of drink would you like to buy? Please enter in the number for your choice of drink? ")
+while order != 0:
+  if order == "1":
+    print("Flat white has been added to your order")
+    print("How many Flath white would you like to order?")
+    order = 0
+  elif order == "2":
+    print("Cappucino has been added to your order")
+    quantity = input("How many Cappucino would you like to order?")
+    print(quantity,"Cappucino(s) has been added to your order.")
+    order = 0
+  elif order == "3":
+    print("Latte has been added to your order")
+    order = 0
+  elif order == "4":
+    print("Decaf has been added to your order")
+    order = 0
+  elif order == "5":
+    print("Hot chocolatte has been added to your order")
+    order = 0
+  elif order == "6":
+    print("order is now complete ")
+    order = 0
 
-def orders ():
-   order = input(print("What type of drink would you like to buy? Please enter in the number for your choice of drink?"))
-   while order != 0:
-     if order == "1":
-       print("Flat white has been added to your order")
-     elif order == "2":
-       print("Cappucino has been added to your order")
-       order = 0
-     elif order == "3":
-       print("Latte has been added to your order")
-       order = 0
-     elif order == "4":
-       print("Decaf has been added to your order")
-       order = 0
-     elif order == "5":
-       print("Hot chocolatte has been added to your order")
-       order = 0
-     elif order == "6":
-       print("order is now complete ")
-       order = 0
-   else:
-     print("sorry but we do not have that in our menu ")
-     order = 0
-  
-order = input(print("What type of drink would you like to buy? Please enter in the number for your choice of drink?"))
+order = input("What type of drink would you like to buy? Please enter in the number for your choice of drink?")
 
 #asking the name of the customer for the receipt
-name = input(print("What would be the name for the order?"))
-name = input(print("Is your name", name,"? Please press 1 for YES and 2 for NO"))
+
+name = input("What would be the name for the order? ")
+name = input("Is your name", name,"? Please press 1 for YES and 2 for NO")
 while name !=0:
   if name == "1":
     print("Thank you for conferming")
+    name = 0
   elif name == "2":
     print("Sorry would you mind typing it again please")
     name = 0
