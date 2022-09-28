b251040e645464ba931eaf33a9cf93d7e02495c8
LawrenceV1 <110138752+coolredz1@users.noreply.github.com>
2022-09-28 22:20:14 +0000

    old go back to if needed

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ def menu ():
   print("|(5)Hot Chocolatte       $4.00|")
   print("|(6)Would like to finish order|")
   print("===============================")
+
+
   
 #print the title here, then print the menu after it. 
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -28,47 +30,71 @@ print("               BUY MY BROWN DRINK               ")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 menu ()
 
+print()
+
 #make an input about what type of coffee they would like 
 order = input("What type of drink would you like to buy? Please enter in the number for your choice of drink? ")
+print()
+
+
+
 while order != 0:
   if order == "1":
     print("Flat white has been added to your order")
-    print("How many Flath white would you like to order?")
+    quantity = int(input("How many Flat White(s) would you like to order?"))
+    print()
+    print(quantity,"Flat White(s) have been added to your order")
     order = 0
   elif order == "2":
     print("Cappucino has been added to your order")
-    quantity = input("How many Cappucino would you like to order?")
+    quantity = int(input("How many Cappucino would you like to order? "))
+    print()
     print(quantity,"Cappucino(s) has been added to your order.")
     order = 0
   elif order == "3":
     print("Latte has been added to your order")
+    quantity = int(input("how many Latte(s) would you like to order?  "))
+    print()
+    print(quantity,"Latte(s) has been added to your order      ")
     order = 0
   elif order == "4":
     print("Decaf has been added to your order")
+    quantity = int(input("How many Decaf(s) woukd you like to buy? "))
+    print()
+    print(quantity,"has Decaf(s) been added to your order.")
     order = 0
   elif order == "5":
     print("Hot chocolatte has been added to your order")
-    order = 0
-  elif order == "6":
+    quantity = int(input("How many Hot chocolate(s) would you like to be buy? "))
+    print()
+    print(quantity,"has hot chocolatte(s) been added to your order. ")
+    order = 0 
+  elif order == "0":
     print("order is now complete ")
     order = 0
+  else:
+    print("Please pick one on of the options in our menu")
+    order = input("What type of drink would you like to buy? Please enter in the number for your choice of drink? ")
 
-order = input("What type of drink would you like to buy? Please enter in the number for your choice of drink?")
-
+order = input("What type of drink would you like to buy? Please enter in the number for your choice of drink? ")
 #asking the name of the customer for the receipt
 
 name = input("What would be the name for the order? ")
-name = input("Is your name", name,"? Please press 1 for YES and 2 for NO")
+name = input(print("Is your name", name,"? Please press 1 for YES and 2 for NO"))
 while name !=0:
-  if name == "1":
+  if name == "0":
     print("Thank you for conferming")
     name = 0
   elif name == "2":
     print("Sorry would you mind typing it again please")
+    name = input("What would the name be for the order? ")
+    name = input("Is your name,", name,"? please press 1 for YES and 2 for NO.")
     name = 0
   else:
     print("sorry invalid choice, please press 1 for YES and 2 for NO")
-    name = 0
+    name = input("What would be the name for the order? ")
+    name = input("Is your name", name,"? Please press 1 for YES and 2 for NO")
+    name = 0 
 
-#Receipt process and asking how many coffee they would like to have 
 
+#Receipt process and asking how many coffee they would like to have this will aslo show the total for each of the 
